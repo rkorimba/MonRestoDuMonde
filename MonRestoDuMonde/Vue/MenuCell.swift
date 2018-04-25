540bf6a5d0adc3c1dced1fb105679a6f4232efa1
rkorimba <korimbacus.riad@gmail.com>
2018-04-25 16:07:08 +0200

    Ajout CollectionView

---
+++ MonRestoDuMonde/Vue/MenuCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCell.swift
+//  MonRestoDuMonde
+//
+//  Created by Riad Korimbacus on 25/04/2018.
+//  Copyright © 2018 Riad Korimbacus. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+    
+}
