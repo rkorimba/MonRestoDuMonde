c9533ebfe4dfbd799f92618da5d391d987479b7f
rkorimba <korimbacus.riad@gmail.com>
2018-04-25 15:51:55 +0200

    Mon Navigation Controlleur

--- MonRestoDuMonde/Controller/MenuController.swift
+++ MonRestoDuMonde/Controller/MenuController.swift
@@ -13,14 +13,10 @@ class MenuController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = "Les Menus du monde"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+   
 
 
 }
---
+++ MonRestoDuMonde/Controller/MonNav.swift
@@ -0,0 +1,21 @@
+//
+//  MonNav.swift
+//  MonRestoDuMonde
+//
+//  Created by Riad Korimbacus on 25/04/2018.
+//  Copyright © 2018 Riad Korimbacus. All rights reserved.
+//
+
+import UIKit
+
+class MonNav: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationBar.barTintColor = NOIR
+        navigationBar.tintColor = GRIS_TRES_CLAIR
+        navigationBar.titleTextAttributes = [.font: FONT_DE_BASE as? Any, .foregroundColor: GRIS_TRES_CLAIR]
+    }
+
+ 
+}
