246d473fce6d2abd17771d164e84c1bd644427c9
rkorimba <korimbacus.riad@gmail.com>
2018-04-25 15:33:09 +0200

    Ajout des controlleurs principaux

--- MonRestoDuMonde/Controller/MenuController.swift
+++ MonRestoDuMonde/Controller/MenuController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MenuController.swift
 //  MonRestoDuMonde
 //
 //  Created by Riad Korimbacus on 25/04/2018.
@@ -8,18 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MenuController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
 
 }
-
