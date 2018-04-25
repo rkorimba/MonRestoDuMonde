750bf409dc5b6a143c71c7f0e1700f08e236c50e
rkorimba <korimbacus.riad@gmail.com>
2018-04-25 15:41:17 +0200

    TabBarCustom

---
+++ MonRestoDuMonde/Controller/MonTabBar.swift
@@ -0,0 +1,20 @@
+//
+//  MonTabBar.swift
+//  MonRestoDuMonde
+//
+//  Created by Riad Korimbacus on 25/04/2018.
+//  Copyright © 2018 Riad Korimbacus. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIR
+    }
+
+    
+}
---
+++ MonRestoDuMonde/Utilitaire/Constantes.swift
@@ -0,0 +1,15 @@
+//
+//  Constantes.swift
+//  MonRestoDuMonde
+//
+//  Created by Riad Korimbacus on 25/04/2018.
+//  Copyright © 2018 Riad Korimbacus. All rights reserved.
+//
+
+import UIKit
+
+let NOIR = UIColor.black
+let GRIS_TRES_FONCE = UIColor(red: 30 / 255, green: 30 / 255, blue: 30 / 255, alpha: 1)
+let GRIS_TRES_CLAIR = UIColor(red: 240 / 255, green: 240 / 255, blue: 240 / 255, alpha: 1)
+
+
