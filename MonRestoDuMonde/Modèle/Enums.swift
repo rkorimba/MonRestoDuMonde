377743d49453ee0ac53738726e1eb7cf35df956e
rkorimba <korimbacus.riad@gmail.com>
2018-04-25 16:14:11 +0200

    Class Plat

---
+++ MonRestoDuMonde/Modèle/Enums.swift
@@ -0,0 +1,15 @@
+//
+//  Enums.swift
+//  MonRestoDuMonde
+//
+//  Created by Riad Korimbacus on 25/04/2018.
+//  Copyright © 2018 Riad Korimbacus. All rights reserved.
+//
+
+import Foundation
+
+enum Type {
+    case entree
+    case platPrincipal
+    case dessert
+}
---
+++ MonRestoDuMonde/Modèle/Plat.swift
@@ -0,0 +1,41 @@
+//
+//  Plat.swift
+//  MonRestoDuMonde
+//
+//  Created by Riad Korimbacus on 25/04/2018.
+//  Copyright © 2018 Riad Korimbacus. All rights reserved.
+//
+
+import UIKit
+
+
+class Plat {
+    
+    private var _type: Type
+    private var _pays: String
+    private var _desc: String
+    private var _image: UIImage
+    
+    var type: Type {
+        return _type
+    }
+    
+    var pays: String {
+        return _pays
+    }
+    
+    var desc: String {
+        return _desc
+    }
+    
+    var image: UIImage {
+        return _image
+    }
+    
+    init(type: Type, pays: String, desc: String, image: UIImage) {
+        self._type = type
+        self._pays = pays
+        self._desc = desc
+        self._image = image
+    }
+}
