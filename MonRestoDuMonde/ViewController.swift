520a848ac5e855cd8be886cb33deff5f822b1089
rkorimba <korimbacus.riad@gmail.com>
2018-04-25 15:14:18 +0200

    Initial Commit

---
+++ MonRestoDuMonde/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MonRestoDuMonde
+//
+//  Created by Riad Korimbacus on 25/04/2018.
+//  Copyright © 2018 Riad Korimbacus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
